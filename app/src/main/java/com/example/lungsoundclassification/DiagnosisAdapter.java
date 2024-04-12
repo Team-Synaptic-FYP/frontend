22896d79e4d31db0dfe7a9fe8428f7d8d81a1f92
supun64 <l.w.supunmadusanka64@gmail.com>
2024-04-12 22:37:45 +0530

    Added pop up population funtionality

--- app/src/main/java/com/example/lungsoundclassification/DiagnosisAdapter.java
+++ app/src/main/java/com/example/lungsoundclassification/DiagnosisAdapter.java
@@ -16,6 +16,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,10 +34,33 @@ public class DiagnosisAdapter extends RecyclerView.Adapter<DiagnosisAdapter.View
     private BlurLayout blurView;
     private DiagnosisActivity diagnosisActivity;
 
+    private ArrayList<String> about_list;
+
     public DiagnosisAdapter(List<DiagnosisModel> diagnosisList, DiagnosisActivity diagnosisActivity) {
         this.diagnosisList = diagnosisList;
         overlayView = diagnosisActivity.findViewById(R.id.pop_up_overlay);
         this.diagnosisActivity = diagnosisActivity;
+
+        this.about_list = new ArrayList<>();
+
+        // Source : https://my.clevelandclinic.org/health/articles/4022-upper-respiratory-infection
+        // Pleural Effusion
+        this.about_list.add("\tPleural effusion, which some people call “water on the lungs,” " +
+                "is the buildup of excess fluid between the layers of the pleura outside your lungs. " +
+                "The pleura are thin membranes that line your lungs and the inside of " +
+                "your chest cavity." +
+                "\n\n" +
+                "\tNormally, everyone has a small amount of fluid in their pleura. This fluid acts " +
+                "as a natural lubricant and makes it easier for your lungs to move when " +
+                "you breathe. But with pleural effusion, you have too much fluid around your lungs. " +
+                "This means your body is producing too much of the " +
+                "fluid or not absorbing enough of the fluid it makes.\n");
+
+        // URTI
+        this.about_list.add("\tA Upper Respiratory Tract Infection (URTI) affects the respiratory system, " +
+                "the part of your body responsible for breathing. These infections can affect " +
+                "your sinuses, throat, lungs or airways. There are two types of respiratory " +
+                "infections\n");
     }
 
     @NonNull
@@ -56,8 +82,7 @@ public class DiagnosisAdapter extends RecyclerView.Adapter<DiagnosisAdapter.View
         holder.itemView.setOnClickListener(v -> {
             // When the item is clicked, show the descriptive pop-up
             // Call the method to show the AlertDialog
-//            showDescriptionDialog(v.getContext(), diagnosis.getDisease(), diagnosis.getConfidentLevel());
-            showDialog(v.getContext());
+            showDialog(v.getContext(), diagnosis.getDisease());
         });
 
         // Add more bindings as needed
@@ -80,9 +105,27 @@ public class DiagnosisAdapter extends RecyclerView.Adapter<DiagnosisAdapter.View
         }
     }
 
-    private void showDialog(Context context){
+    private void showDialog(Context context, String disease){
         Dialog dialog = new Dialog(context, R.style.DialogStyle);
+
+
         dialog.setContentView(R.layout.dialog_custom_layout);
+
+        TextView title = dialog.findViewById(R.id.txt_title);
+        TextView about = dialog.findViewById(R.id.txt_about);
+
+        title.setText(disease);
+
+        switch (disease){
+            case "Pleural Effusion":
+                about.setText(about_list.get(0));
+                break;
+            case "URTI":
+                about.setText(about_list.get(1));
+                break;
+        }
+
+
 
 
         // Configure the blur effect
